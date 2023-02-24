5fdf5d8745ff8d8aae93cdea92ff9c4749a3ab1b
Евгения <ev.chernova@icloud.com>
2023-02-25 00:51:53 +0300

    Solution for task3 added

--- seminar2/task3.py
+++ seminar2/task3.py
@@ -2,3 +2,11 @@
 Реализовать проверку типа данных каждого элемента.
 Использовать функцию type() для проверки типа.
 Элементы списка можно не запрашивать у пользователя, а указать явно, в программе."""
+
+one_list = [5.6, False, None, -1, 'helloWorld',  4]
+def check_type(i):
+     for i in range(len(one_list)):
+         print(type(one_list[i]))
+     return
+print(one_list)
+check_type(one_list)
