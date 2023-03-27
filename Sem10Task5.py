ad2fae8724e6bd86d53cd07c321498bda80f3934
Евгения <ev.chernova@icloud.com>
2023-03-28 01:03:12 +0300

    Solution for tasks 1,2,3,4,5 in sem10 added

--- Sem10Task5.py
+++ Sem10Task5.py
@@ -1,2 +1,21 @@
 """Выполнить пинг веб-ресурсов yandex.ru, youtube.com и
  преобразовать результаты из байтовового в строковый тип на кириллице."""
+
+import subprocess
+import chardet
+
+args = ['ping', '-c', '2', 'yandex.ru']
+
+yandex_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+for line in yandex_ping.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result["encoding"]).encode("utf-8")
+    print(line.decode("utf-8"))
+
+args = ['ping', '-c', '1', 'youtube.com']
+
+youtube_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+for line in youtube_ping.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result["encoding"]).encode("utf-8")
+    print(line.decode("utf-8"))
